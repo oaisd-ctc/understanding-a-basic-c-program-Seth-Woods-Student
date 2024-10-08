2ddcc0cdd7e0cf9533ae5895261bbdf0b5e33d15
Seth-Woods-Student <sewoae4@student.oaisd.org>
2024-10-08 13:56:00 +0000

    finished

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class Program
+{
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine("hello world!");
+        Console.WriteLine("my name is seth woods.");
+
+        int age = 16;
+        string greeting = "hello world!";
+        string firstname = "seth";
+        string lastname = "woods";
+        double money = 3.00;
+
+        Console.WriteLine("greetings" + "!" + " my name is " + firstname + " " + lastname + ". I am " + age + " years old. I just spent $" + money + " on an energy drink");
+    }
+}
